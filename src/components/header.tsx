ff87d041272b2b05afaa51be49002ec34f2af572
abdalrahmanf2 <abdalrahman.vim@gmail.com>
2025-07-29 11:12:22 +0300

    refactor: header links active state

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
 import { Caret } from "./caret";
+import { usePathname } from "next/navigation";
+import { cn } from "~/lib/utils";
+
+const NAV_LINKS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Blog",
+    href: "/blog",
+  },
+];
 
 export function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="bg-background/80 sticky top-2 z-50 flex items-center justify-between border-2 px-6 py-3 backdrop-blur">
       <Link href="/" className="flex items-center gap-4">
@@ -26,17 +43,17 @@ export function Header() {
       <div className="flex items-center gap-3">
         <nav className="hidden md:block">
           <ul className="flex items-center">
-            <li>
-              <Button variant="link" asChild>
-                <Link href="/">Home</Link>
-              </Button>
-            </li>
-
-            <li>
-              <Button variant="link" asChild>
-                <Link href="/blog">Blog</Link>
-              </Button>
-            </li>
+            {NAV_LINKS.map((link) => (
+              <li key={link.href}>
+                <Button
+                  variant="link"
+                  asChild
+                  className={cn(pathname === link.href && "text-chart-4")}
+                >
+                  <Link href={link.href}>{link.label}</Link>
+                </Button>
+              </li>
+            ))}
           </ul>
         </nav>
         <ModeToggle />

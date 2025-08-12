0c062d2a16178197409d3676ce542d39f7302bfd
abdalrahmanf2 <abdalrahman.vim@gmail.com>
2025-08-12 15:08:46 +0300

    fix: padding in cta

--- src/app/(home)/_components/cta.tsx
+++ src/app/(home)/_components/cta.tsx
@@ -10,24 +10,26 @@ import {
 
 export function CTA() {
   return (
-    <Card>
-      <CardHeader className="mx-auto w-full max-w-xl text-center">
-        <CardTitle className="text-2xl font-bold">
-          Have an idea? I can take it from “what if” to shipped.
-          <br />
-        </CardTitle>
-        <CardDescription>
-          (Hire me before I go back to tweaking my Arch rice.)
-        </CardDescription>
-      </CardHeader>
-      <CardFooter className="justify-center">
-        <Button className="w-full max-w-xs" asChild>
-          <a href="mailto:abdalrahman.vim@gmail.com">
-            Contact me
-            <ArrowUpRight />
-          </a>
-        </Button>
-      </CardFooter>
-    </Card>
+    <section className="mb-24 pt-12">
+      <Card>
+        <CardHeader className="mx-auto w-full max-w-xl text-center">
+          <CardTitle className="text-2xl font-bold">
+            Have an idea? I can take it from “what if” to shipped.
+            <br />
+          </CardTitle>
+          <CardDescription>
+            (Hire me before I go back to tweaking my Arch rice.)
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="justify-center">
+          <Button className="w-full max-w-xs" asChild>
+            <a href="mailto:abdalrahman.vim@gmail.com">
+              Contact me
+              <ArrowUpRight />
+            </a>
+          </Button>
+        </CardFooter>
+      </Card>
+    </section>
   );
 }
